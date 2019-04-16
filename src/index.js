4c2d3dc26b13d0621e40d266d7cf771eb74f061a
s-seguin <stuart.seguin@ucalgary.ca>
2019-04-15 19:00:00 -0600

    trying to use .env but cors causing issues

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Homepage from "./components/Homepage";
+//require('dotenv').config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
